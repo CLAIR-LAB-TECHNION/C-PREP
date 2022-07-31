e708a8b94d7113e01ab013ddf3ad110ffd309f35
Guy Azran <guy.azran@mail.huji.ac.il>
2022-08-01 01:03:00 +0300

    feat: linear and step lr schedulers

---
+++ rmrl/utils/lr_schedulers.py
@@ -0,0 +1,27 @@
+from typing import Dict
+
+from stable_baselines3.common.type_aliases import Schedule
+
+
+def linear_schedule(initial_value: float) -> Schedule:
+    """
+    from:
+    https://stable-baselines3.readthedocs.io/en/master/guide/examples.html#learning-rate-schedule
+    """
+    def func(progress_remaining: float) -> float:
+        return progress_remaining * initial_value
+
+    return func
+
+
+def step_schedule(initial_value: float, steps_map: Dict[float, float]) -> Schedule:
+    cur_value = [initial_value]  # use immutable type for pointer capabilities
+    step_points = sorted(steps_map.keys())
+
+    def func(progress_remaining: float) -> float:
+        if step_points[-1] > progress_remaining:
+            cur_value[0] = steps_map[step_points.pop()]
+
+        return cur_value[0]
+
+    return func
