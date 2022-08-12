d1082086631f41454bd160304f143ce6a5be67c4
Guy Azran <guy.azran@mail.huji.ac.il>
2022-08-12 22:22:29 +0300

    cleanup: setup slurm jobs scripts for run and batch separately

--- scripts/slurm_job.sh
+++ scripts/slurm_job.sh
@@ -3,15 +3,16 @@
 ###
 # Parameters for sbatch
 #
+COMMAND=$1  # valid values are srun, sbatch
+JOB_NAME=$2
 NUM_NODES=1
-NUM_CORES=$2
-NUM_GPUS=$3
-JOB_NAME=$1
+NUM_CORES=$3
+NUM_GPUS=$4
 MAIL_USER="guy.azran@campus.technion.ac.il"
 MAIL_TYPE=ALL # Valid values are NONE, BEGIN, END, FAIL, REQUEUE, ALL
 
 # move num gpus argument
-shift 3
+shift 4
 
 ###
 # Conda parameters
@@ -19,7 +20,7 @@ shift 3
 CONDA_HOME=$HOME/miniconda3
 CONDA_ENV=rmrl
 
-sbatch \
+$COMMAND \
 	-N $NUM_NODES \
 	-c $NUM_CORES \
 	--gres=gpu:$NUM_GPUS \
@@ -37,8 +38,8 @@ echo "*** Activating environment $CONDA_ENV ***"
 source $CONDA_HOME/etc/profile.d/conda.sh
 conda activate $CONDA_ENV
 
-# Run python with the args to the script
-python $@
+# Run script with arguments
+$@
 
 echo "*** SLURM BATCH JOB '$JOB_NAME' DONE ***"
 EOF
