adab89deaf269ed09516701448bc0295a82f7f8b
Guy Azran <guy.azran@mail.huji.ac.il>
2022-08-23 11:59:16 +0300

    Dev: dataset generation and epoch training

--- rmrl/nn/pretraining.py
+++ rmrl/nn/pretraining.py
@@ -6,6 +6,7 @@ from typing import Union
 import numpy as np
 import torch
 from gym.spaces import MultiBinary, Box
+from sklearn.model_selection import train_test_split
 from torch import nn
 from torch.optim import Adam
 from torch_geometric.data import Batch
@@ -16,16 +17,19 @@ from tqdm.auto import tqdm
 from rmrl.nn.models import MultilayerGNN
 from rmrl.reward_machines.potential_functions import ValueIteration
 from rmrl.reward_machines.reward_machine import RMFromNX, RewardMachine
-from rmrl.utils.custom_spaces import PygData
+from rmrl.reward_machines.rm_env import RM_DATA_KEY, CUR_STATE_PROPS_KEY
 from rmrl.utils.misc import debatch_graph_to_specific_node
+
+from stable_baselines3.common.custom_spaces import PygData
 
 NODE_ATTR_KEY = 'x'
 EDGE_ATTR_KEY = 'edge_attr'
 
-RM_GEN_OUTPUT_PYG = 'pyg'
-RM_GEN_OUTPUT_NX = 'nx'
-RM_GEN_OUTPUT_RM = 'rm'
-RM_GEN_OUTPUT_ALL = 'all'
+HIGHEST_VALUE_PATH_KEY = 'highest_value_path'
+
+GRPT_DIR = Path('grpt')
+GRPT_DS_DIR = GRPT_DIR / 'ds'
+GRPT_MODELS_DIR = GRPT_DIR / 'models'
 
 
 class RMGraphGenerator:
@@ -49,9 +53,12 @@ class RMGraphGenerator:
     def sample_batch(self):
         return [self.sample_one() for _ in range(self.batch_size)]
 
-    def sample(self, n):
+    def sample(self, n, verbose=False):
         out = []
-        for _ in range(n):
+        iter_range = range(n)
+        if verbose:
+            iter_range = tqdm(iter_range, desc='generating RMs')
+        for _ in iter_range:
             if self.batch_size is None:
                 out.append(self.sample_one())
             else:
@@ -84,6 +91,64 @@ def highest_value_path(rm: RewardMachine, start, max_path_length, reshape=True, 
         start = next_node
 
     return path
+
+
+# class HVPathDataset(Dataset):
+#     def __init__(self, size, test_frac, num_propositions, max_nodes, max_seq_len, gamma, seed, **gen_kwargs):
+#         ds_path = get_grpt_dir(GRPT_DS_DIR, size=size, test_frac=test_frac,
+#                                num_propositions=num_propositions, max_nodes=max_nodes,
+#                                max_seq_len=max_seq_len, gamma=gamma, **gen_kwargs) / 'ds.pkl'
+#         if ds_path.exists():
+#
+#
+#     @staticmethod
+#     def get_grpt_dir(base, **kwargs):
+#         p = base
+#         for k, v in kwargs.items():
+#             p /= f'{k}-{v}'
+#         return p
+
+
+def generate_dataset(size, test_frac, num_propositions, max_nodes, max_seq_len, gamma, seed, **gen_kwargs):
+    gen = RMGraphGenerator(num_propositions, max_nodes, seed=seed, **gen_kwargs)
+    sample = gen.sample(size, verbose=True)
+    eos_token = torch.full((num_propositions,), 0.)
+
+    ds_dicts = []
+    for rm in tqdm(sample, desc='preparing dataset'):
+        state_list = list(rm.all_states)
+        cur_state = state_list[np.random.choice(range(len(state_list)))]
+        hv_path_from_cur_state = highest_value_path(rm, cur_state, max_seq_len, gamma=gamma)
+        pyg_data = rm.to_pyg_data()  # !! do this AFTER `highest_value_path` for reward shaping
+
+        ds_dicts.append({
+            CUR_STATE_PROPS_KEY: torch.tensor(cur_state),
+            HIGHEST_VALUE_PATH_KEY: pad_path(hv_path_from_cur_state, max_seq_len, eos_token),
+            RM_DATA_KEY: pyg_data
+        })
+
+    train_set, test_set = train_test_split(ds_dicts, test_size=test_frac)
+
+    save_dir = get_grpt_dir(GRPT_DS_DIR, size=size, test_frac=test_frac, num_propositions=num_propositions,
+                            max_nodes=max_nodes, max_seq_len=max_seq_len, gamma=gamma, seed=seed, **gen_kwargs)
+    save_dir.mkdir(parents=True, exist_ok=True)
+
+    torch.save((train_set, test_set), save_dir / 'ds.pkl')
+
+
+def pad_path(path, max_seq_len, pad_value):
+    required_padding = (max_seq_len - len(path))
+    padded_path = path + ([pad_value] * required_padding)
+    return torch.tensor(padded_path).float()
+    # required_padding = (max_seq_len - len(path))
+    # return torch.cat([path, torch.tensor([pad_value] * required_padding)])
+
+
+def get_grpt_dir(base, **kwargs):
+    p = base
+    for k, v in kwargs.items():
+        p /= f'{k}-{v}'
+    return p
 
 
 class HighestValueNet(nn.Module):
@@ -141,93 +206,75 @@ class HighestValueNet(nn.Module):
 
 
 class RMGNNTrainer:
-    def __init__(self,
-                 rm_gen: RMGraphGenerator,
-                 model: HighestValueNet):
-        self.rm_gen = rm_gen
+    def __init__(self, model: HighestValueNet):
         self.model = model
-
         self.loss_fn = nn.BCEWithLogitsLoss()
 
         self.history = None
 
     def __reset_history(self):
         self.history = {
-            'iteration': [],
+            'epoch': [],
             'training_loss': [],
             'validation_loss': []
         }
 
-    def train(self, num_iters, learning_rate, print_every=100):
+    def train(self, dl_train, dl_val, num_epochs, learning_rate, print_every=100):
         try:
             self.__reset_history()
 
             optimizer = Adam(self.model.parameters(), lr=learning_rate)
-            for i, (train_batch, eval_batch) in tqdm(enumerate(zip(self.rm_gen, self.rm_gen), 1), total=num_iters):
-                train_loss = self.train_batch(train_batch, optimizer)
-                eval_loss = self.eval_batch(eval_batch)
-                self.history['iteration'].append(i)
+            for epoch in tqdm(range(1, num_epochs + 1), position=0):
+                train_loss = self.train_epoch(dl_train, optimizer)
+                eval_loss = self.eval_epoch(dl_val)
+                self.history['epoch'].append(epoch)
                 self.history['training_loss'].append(train_loss)
                 self.history['validation_loss'].append(eval_loss)
 
-                if i % print_every == 0:
-                    print(f'iteration   {i}')
+                if epoch % print_every == 0:
+                    print(f'epoch   {epoch}')
                     print(f'train loss: {train_loss}')
                     print(f'eval loss:  {eval_loss}')
                     print('==================================================')
                     print()
-
-                if i == num_iters:
-                    break
 
         except KeyboardInterrupt:
             print('training interrupted by user')
 
         return self.history
 
-    def prep_batch(self, rm_batch):
-        cur_states = []
-        highest_value_paths = []
-        pyg_datas = []
-        for rm in rm_batch:
-            state_list = list(rm.all_states)
-            cur_states.append(state_list[np.random.choice(range(len(state_list)))])
-            highest_value_paths.append(highest_value_path(rm, cur_states[-1], self.model.max_seq_len))
-            pyg_datas.append(rm.to_pyg_data())  # !! do this AFTER `highest_value_path` for reward shaping
+    def train_epoch(self, dl, optimizer):
+        batch_losses = []
+        for batch in tqdm(dl, desc='training epoch batches', leave=False, position=1):
+            batch_loss = self.train_batch(batch, optimizer)
+            batch_losses.append(batch_loss)
 
-        pyg_batch = Batch.from_data_list(pyg_datas)
-        cur_state_batch = torch.tensor(cur_states)
-        highest_value_paths_batch = self.__handle_highest_value_paths(highest_value_paths)
-
-        return pyg_batch, cur_state_batch, highest_value_paths_batch
-
-    def __handle_highest_value_paths(self, highest_value_paths):
-        max_seq_len = max(map(len, highest_value_paths))
-        if max_seq_len < self.model.max_seq_len:
-            max_seq_len += 1  # if all sequences terminate, make sure they have at least one end-of-seq token
-        for p in highest_value_paths:
-            required_padding = (max_seq_len - len(p))
-            p.extend([self.model.eos_token] * required_padding)
-        return torch.tensor(highest_value_paths).float()
+        return np.mean(batch_losses)
 
     def train_batch(self, rm_batch, optimizer):
         self.model.train()
-        pyg_datas, cur_states, paths = self.prep_batch(rm_batch)
-        logits = self.model(pyg_datas, cur_states, paths)
-        loss = self.loss_fn(logits, paths)
+        logits = self.model(rm_batch[RM_DATA_KEY], rm_batch[CUR_STATE_PROPS_KEY], rm_batch[HIGHEST_VALUE_PATH_KEY])
+        loss = self.loss_fn(logits, rm_batch[HIGHEST_VALUE_PATH_KEY])
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
         return loss.item()
 
+    def eval_epoch(self, dl):
+        batch_losses = []
+        for batch in tqdm(dl, desc='evaluation epoch batches', leave=False, position=1):
+            batch_loss = self.eval_batch(batch)
+            batch_losses.append(batch_loss)
+
+        return np.mean(batch_losses)
+
     def eval_batch(self, rm_batch):
         self.model.eval()
 
         with torch.no_grad():
-            pyg_datas, cur_states, paths = self.prep_batch(rm_batch)
-            logits = self.model(pyg_datas, cur_states, paths)
-            loss = self.loss_fn(logits, paths)
+            logits = self.model(rm_batch[RM_DATA_KEY], rm_batch[CUR_STATE_PROPS_KEY], rm_batch[HIGHEST_VALUE_PATH_KEY])
+            loss = self.loss_fn(logits, rm_batch[HIGHEST_VALUE_PATH_KEY])
 
         return loss.item()
 
@@ -252,5 +299,5 @@ def pretrain_graph_embedding(num_props, max_nodes, num_iters, batch_size, learni
     rm_gen = RMGraphGenerator(num_props, max_nodes, batch_size=batch_size, seed=seed)
     model = HighestValueNet(input_size=rm_gen.graph_space.nf_space.n)
     trainer = RMGNNTrainer(rm_gen, model)
-    history = trainer.train(num_iters, learning_rate)
+    history = trainer.train(num_iters, learning_rate, print_every)
     trainer.save_all(save_dir)
