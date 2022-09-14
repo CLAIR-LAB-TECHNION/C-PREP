773accc97e3b6d9e6082e85c847ad6d95950f6c0
Guy Azran <guy.azran@mail.huji.ac.il>
2022-09-14 13:09:24 +0300

    Dev: more compatible lr schedulers

--- rmrl/utils/lr_schedulers.py
+++ rmrl/utils/lr_schedulers.py
@@ -1,27 +1,47 @@
+from abc import ABC, abstractmethod
 from typing import Dict
 
 from stable_baselines3.common.type_aliases import Schedule
 
 
-def linear_schedule(initial_value: float) -> Schedule:
-    """
-    from:
-    https://stable-baselines3.readthedocs.io/en/master/guide/examples.html#learning-rate-schedule
-    """
-    def func(progress_remaining: float) -> float:
-        return progress_remaining * initial_value
+class LearningRateSchedule(ABC):
+    @abstractmethod
+    def __call__(self, progress_remaining: float) -> float:
+        pass
 
-    return func
+    def __repr__(self):
+        vars_join = ', '.join(f'{k}={v}'
+                              for k, v in vars(self).items()
+                              if not k.startswith(f'_{self.__class__.__name__}'))
+        return f'{self.__class__.__name__}({vars_join})'
+
+    def __eq__(self, other):
+        return(isinstance(other, self.__class__) and
+               vars(self) == vars(other))
+
+    def __hash__(self):
+        return hash(tuple(vars(self).items()))
 
 
-def step_schedule(initial_value: float, steps_map: Dict[float, float]) -> Schedule:
-    cur_value = [initial_value]  # use immutable type for pointer capabilities
-    step_points = sorted(steps_map.keys())
+class LinearSchedule(LearningRateSchedule):
+    def __init__(self, initial_value: float):
+        self.initial_value = initial_value
 
-    def func(progress_remaining: float) -> float:
-        if step_points and step_points[-1] > progress_remaining:
-            cur_value[0] = steps_map[step_points.pop()]
+    def __call__(self, progress_remaining: float) -> float:
+        return progress_remaining * self.initial_value
 
-        return cur_value[0]
 
-    return func
+class StepSchedule(LearningRateSchedule):
+    def __init__(self, initial_value: float, steps_map: Dict[float, float]):
+        self.initial_value = initial_value
+        self.steps_map = steps_map
+        self.__step_points = sorted(steps_map.keys(), reverse=True)
+
+    def __call__(self, progress_remaining: float) -> float:
+        prev_p = 1
+        for p in self.__step_points:
+            if p < progress_remaining:
+                return self.steps_map.get(prev_p, self.initial_value)
+            prev_p = p
+
+        return self.steps_map[self.__step_points[-1]]
