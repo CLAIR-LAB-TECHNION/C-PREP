e3b8a9bd262c512c0a42757400410cfea3c4f878
Guy Azran <guy.azran@mail.huji.ac.il>
2022-12-12 21:54:59 +0200

    bugfix: ohe target eval

--- rmrl/experiments/no_transfer.py
+++ rmrl/experiments/no_transfer.py
@@ -7,6 +7,10 @@ class NoTransferExperiment(Experiment):
         env, tgt_env = envs
         eval_env, tgt_eval_env = eval_envs
 
+        # set tgt ohe settings
+        tgt_eval_env.env.env.ohe_start = self.cfg.num_src_samples
+        tgt_eval_env.env.env.set_fixed_contexts(tgt_eval_env.env.env.fixed_contexts)
+
         self._tgt_for_test = tgt_eval_env
 
         self.get_agent_for_env(env, eval_env)
